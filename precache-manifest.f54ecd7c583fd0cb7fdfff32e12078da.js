5b2bb209432346bf492f16dd14241fba53dff84b
Akaevjumgal <akaev.jumgal@gmail.com>
2018-12-09 23:47:01 +0600

    Updates

---
+++ precache-manifest.f54ecd7c583fd0cb7fdfff32e12078da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74f096fbbe7448290acb",
+    "url": "/static/js/main.74f096fb.chunk.js"
+  },
+  {
+    "revision": "8b08cb60163e798a4679",
+    "url": "/static/js/1.8b08cb60.chunk.js"
+  },
+  {
+    "revision": "74f096fbbe7448290acb",
+    "url": "/static/css/main.40e5b8ea.chunk.css"
+  },
+  {
+    "revision": "1b546c75456ba6bc1b0042e61c7110b7",
+    "url": "/index.html"
+  }
+];
